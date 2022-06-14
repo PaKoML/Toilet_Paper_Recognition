a58dd55141b7901498d85a4c489839f8b3ccd532
raphael825 <67963713+kmj0825@users.noreply.github.com>
2022-06-15 01:18:10 +0900

    First Website Code
    매우 단순함 (우리가 만들 사이트에 대한 것보다 붙여넣은 것이 많음)
    Component 부터 차근차근 수정할 예정

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Main from './fileUpload'
+import MainMain from './testUpload'
+
+const App = () => (
+    <div>
+        <h1>Hello, This is Toilet Paper Detection Model</h1>
+        <h3> MADE BY PAKO</h3>
+        <Main/>
+    </div>
+
+);
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
